0af9b7d9db3fa2fcb20805c914ed77eaa1e1c005
satpal82bhandari <satpal82bhandari@gmail.com>
2024-07-09 18:13:32 +0530

    transfer_fee_extention

---
+++ app.ts
@@ -0,0 +1,230 @@
+// Import necessary functions and constants from the Solana web3.js and SPL Token packages
+import {
+    sendAndConfirmTransaction,
+    Connection,
+    Keypair,
+    SystemProgram,
+    Transaction,
+    LAMPORTS_PER_SOL,
+    Cluster,
+    PublicKey,
+} from '@solana/web3.js';
+
+import {
+    ExtensionType,
+    createInitializeMintInstruction,
+    mintTo,
+    createAccount,
+    getMintLen,
+    getTransferFeeAmount,
+    unpackAccount,
+    TOKEN_2022_PROGRAM_ID,
+    createInitializeTransferFeeConfigInstruction,
+    harvestWithheldTokensToMint,
+    transferCheckedWithFee,
+    withdrawWithheldTokensFromAccounts,
+    withdrawWithheldTokensFromMint,
+    getOrCreateAssociatedTokenAccount,
+    createAssociatedTokenAccountIdempotent
+} from '@solana/spl-token';
+
+
+// Initialize connection to local Solana node
+
+
+const connection = new Connection('http://127.0.0.1:8899', 'confirmed');
+
+// Generate keys for payer, mint authority, and mint
+
+const payer = Keypair.generate();
+console.log("payer : ",payer.publicKey , "\n\n");
+
+const mintAuthority = Keypair.generate();
+console.log("mintAuthority : ",mintAuthority.publicKey, "\n\n");
+
+const mintKeypair = Keypair.generate();
+console.log("mint : ",mintKeypair.publicKey, "\n\n");
+
+const mint = mintKeypair.publicKey;
+
+
+// Generate keys for transfer fee config authority and withdrawal authority
+
+
+const transferFeeConfigAuthority = Keypair.generate();
+console.log("transferFeeConfigAuthority : ",transferFeeConfigAuthority.publicKey, "\n\n");
+const withdrawWithheldAuthority = Keypair.generate();
+console.log("withdrawWithheldAuthority : ",withdrawWithheldAuthority.publicKey, "\n\n");
+
+// Define the extensions to be used by the mint
+// Calculate the length of the mint
+
+
+const extensions = [
+    ExtensionType.TransferFeeConfig,
+];
+
+const mintLen = getMintLen(extensions);
+console.log(mintLen, "\n\n");
+
+
+// Set the decimals, fee basis points, and maximum fee
+
+const decimals = 9;
+const feeBasisPoints = 100; // 1%
+const maxFee = BigInt(9 * Math.pow(10, decimals));      // 9 tokens
+
+// Define the amount to be minted and the amount to be transferred, accounting for decimals
+
+const mintAmount = BigInt(1_000_000 * Math.pow(10, decimals)); // Mint 1,000,000 tokens
+const transferAmount = BigInt(1_000 * Math.pow(10, decimals)); // Transfer 1,000 tokens
+
+// Calculate the fee for the transfer
+
+const calcFee = (transferAmount * BigInt(feeBasisPoints)) / BigInt(10_000); // expect 10 token as fee
+const fee = calcFee > maxFee ?  maxFee : calcFee; // expect 9 token as fee
+
+// Helper function to generate Explorer URL
+function generateExplorerTxUrl(txId: string) {
+    return `https://explorer.solana.com/tx/${txId}?cluster=custom&customUrl=http%3A%2F%2Flocalhost%3A8899`;
+}
+
+
+async function main() {
+    
+    // Step 1 - Airdrop to Payer
+
+    const airdropSignature = await connection.requestAirdrop(payer.publicKey, 2 * LAMPORTS_PER_SOL);
+    await connection.confirmTransaction({ signature: airdropSignature, ...(await connection.getLatestBlockhash()) });
+
+    // Step 2 - Create a New Token
+
+
+       const mintLamports = await connection.getMinimumBalanceForRentExemption(mintLen);
+       console.log("rent lamports : ",mintLamports, "\n\n");
+       const mintTransaction = new Transaction().add(
+           SystemProgram.createAccount({
+               fromPubkey: payer.publicKey,
+               newAccountPubkey: mint,
+               space: mintLen,
+               lamports: mintLamports,
+               programId: TOKEN_2022_PROGRAM_ID,
+           }),
+           createInitializeTransferFeeConfigInstruction(
+               mint,
+               transferFeeConfigAuthority.publicKey,
+               withdrawWithheldAuthority.publicKey,
+               feeBasisPoints,
+               maxFee,
+               TOKEN_2022_PROGRAM_ID
+           ),
+           createInitializeMintInstruction(mint, decimals, mintAuthority.publicKey, null, TOKEN_2022_PROGRAM_ID)
+       );
+       const newTokenTx = await sendAndConfirmTransaction(connection, mintTransaction, [payer, mintKeypair], undefined);
+    
+       console.log("New Token Created:", generateExplorerTxUrl(newTokenTx), "\n\n");
+
+    // Step 3 - Mint tokens to Owner
+
+    const sourceWallet = Keypair.generate();
+    console.log("sourceWallet",sourceWallet.publicKey, "\n\n");
+
+    const sourceAccount = await createAssociatedTokenAccountIdempotent(connection, payer, mint, sourceWallet.publicKey, {}, TOKEN_2022_PROGRAM_ID);
+    console.log("sourceAccount",sourceAccount, "\n\n");
+    const mintSig = await mintTo(connection,payer,mint,sourceAccount,mintAuthority,mintAmount,[],undefined,TOKEN_2022_PROGRAM_ID);
+    console.log("Tokens Minted:", generateExplorerTxUrl(mintSig), "\n\n");
+
+
+
+    // Step 4 - Send Tokens from Owner to a New Account
+
+    const destinationWallet = Keypair.generate();
+    const destinationAccount = await createAssociatedTokenAccountIdempotent(connection, payer, mint, destinationWallet.publicKey, {}, TOKEN_2022_PROGRAM_ID);
+    console.log("destination token account",destinationAccount);
+    const transferSig = await transferCheckedWithFee(
+        connection,
+        payer,
+        sourceAccount,
+        mint,
+        destinationAccount,
+        sourceWallet,
+        transferAmount,
+        decimals,
+        fee,
+        []
+    );
+    console.log("Tokens Transferred:", generateExplorerTxUrl(transferSig));
+
+    // Step 5 - Fetch Fee Accounts
+
+    const allAccounts = await connection.getProgramAccounts(TOKEN_2022_PROGRAM_ID, {
+        commitment: 'confirmed',
+        filters: [
+            {
+                memcmp: {
+                    offset: 0,
+                    bytes: mint.toString(),
+                },
+            },
+        ],
+    });
+     
+    const accountsToWithdrawFrom: PublicKey[] = [];
+    for (const accountInfo of allAccounts) {
+        const account = unpackAccount(accountInfo.pubkey, accountInfo.account, TOKEN_2022_PROGRAM_ID);
+        const transferFeeAmount = getTransferFeeAmount(account);
+        if (transferFeeAmount !== null && transferFeeAmount.withheldAmount > BigInt(0)) {
+            accountsToWithdrawFrom.push(accountInfo.pubkey);
+        }
+    };
+    console.log("accountsToWithdrawFrom : ", accountsToWithdrawFrom);
+
+
+    //  // Step 6 - Withdraw Fees by Owner
+
+    // const feeVault = Keypair.generate();
+    // const feeVaultAccount = await createAssociatedTokenAccountIdempotent(connection, payer, mint, feeVault.publicKey, {}, TOKEN_2022_PROGRAM_ID);
+
+    // const harvestSig = await harvestWithheldTokensToMint(connection, payer, mint, [destinationAccount]);
+    // console.log("Harvest by Owner:", generateExplorerTxUrl(harvestSig));
+
+    // // Tokens that have been harvested to Mint can then be claimed by the authority 
+    // const withdrawSig2 = await withdrawWithheldTokensFromMint(
+    //     connection,
+    //     payer,
+    //     mint,
+    //     feeVaultAccount,
+    //     withdrawWithheldAuthority,
+    //     []
+    // );
+
+    // console.log("Withdraw from Mint:", generateExplorerTxUrl(withdrawSig2));
+
+    
+    // --------------------------------------------------------------------------------------------------
+        // Step 6 Withdraw Fees by Authority
+
+
+        const feeVault = Keypair.generate();
+        const feeVaultAccount = await createAssociatedTokenAccountIdempotent(connection, payer, mint, feeVault.publicKey, {}, TOKEN_2022_PROGRAM_ID);
+        console.log("feeVaultAccount : ",feeVaultAccount);
+    
+        const withdrawSig1 = await withdrawWithheldTokensFromAccounts(
+            connection,
+            payer,
+            mint,
+            feeVaultAccount,
+            withdrawWithheldAuthority,
+            [],
+            accountsToWithdrawFrom
+        );
+        console.log("Withdraw from Accounts:", generateExplorerTxUrl(withdrawSig1));
+        
+        
+        // ---------------------------------------------------------------------------------------------
+        
+        
+    }
+
+// Execute the main function
+main();
